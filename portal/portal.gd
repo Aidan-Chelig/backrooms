51d1384cb60fc609add2a5cacbf192ae5b87652f
Aidan Chelig <aidan.chelig@gmail.com>
2022-05-02 23:39:31 -0500

    weird fix2

--- portal/portal.gd
+++ portal/portal.gd
@@ -6,44 +6,36 @@ extends Spatial
 # var a = 2
 # var b = "text"
 
-export var follow_cam_input: NodePath setget fcs
-export var centered_around_input: Vector3
+export(NodePath) onready var follow_cam
+export(NodePath) onready var reference_node
 
-onready var mask_cam = $mask/maskRoot/maskCam
+
 onready var portal_cam = $portalView/Handle/portalCam
 onready var body = $body
 
-onready var mask_view = $mask
 onready var portal_view = $portalView
-
-
-onready var maskroot = $mask/maskRoot
+onready var bodytracker = $bodyTracker
+onready var camtracker = $bodyTracker/camTracker;
 
 onready var handle = $portalView/Handle
 
-var follow_cam: Camera;
-
-func fcs(newfcs):
-	follow_cam = get_node(newfcs)
-	follow_cam_input = newfcs
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_tree().root.connect("size_changed", self, "_on_viewport_size_changed");
-	if follow_cam_input:
-		follow_cam = get_node(follow_cam_input)
+	handle.set_disable_scale(true)
+	bodytracker.set_disable_scale(true)
+	camtracker.set_disable_scale(true)
 
 
 func _on_viewport_size_changed():
 	var size = OS.get_real_window_size();
-	print(size);
 	portal_view.size = size;
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if follow_cam:
-		mask_cam.global_transform = follow_cam.global_transform
-		maskroot.global_transform = body.global_transform;
-		maskroot.scale = Vector3(1.0,1.0,1.0);
-		portal_cam.transform = mask_cam.transform
+	if follow_cam && reference_node:
+		handle.global_transform = get_node(reference_node).global_transform
+		bodytracker.global_transform = body.global_transform
+		camtracker.global_transform = get_node(follow_cam).global_transform
+		portal_cam.transform = camtracker.transform
